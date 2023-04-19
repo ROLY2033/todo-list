add2b0c4ef0d4d74673bfddc00bada0ed8baf728
= <=>
2023-04-18 21:16:03 -0500

    agregado la configucion de bean

--- src/main/java/com/todoapp/todoapp/TodoappApplication.java
+++ src/main/java/com/todoapp/todoapp/TodoappApplication.java
@@ -1,6 +1,7 @@
 package com.todoapp.todoapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationShutdownHandlers;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,7 +10,6 @@ public class TodoappApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(TodoappApplication.class, args);
-
 	}
 
 }
